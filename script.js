526ea84e1d0fda6af5c306b59aa366f26345ad57
RaidDEV-tech <chougnayt@gmail.com>
2026-05-12 20:34:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('%cDiamond Roleplay - Professional RP Experience', 'color: #00d4ff; font-size: 16px;');
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website loaded successfully');
+});
